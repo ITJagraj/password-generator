47b1c1a2e88aed0152effce0f8d2a6418fdd9513
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-03-14 19:53:57 -0400

    minimum count

--- script.js
+++ script.js
@@ -22,4 +22,17 @@ generateBtn.addEventListener("click", writePassword);
 //Promt that box after clicking
 function generatePassword() {
    var passwordLenght = prompt("Please enter the number of characters. It must be more than 8 and less than 128") 
+   var numbers = confirm("Do you want numbers in the password?");
+   var lowerCases = confirm("Do you want lowercases in your password?");
+   var UpperCases = confirm("Do you want uppercases in your password?");
+   var special = confirm("Do you want special charactiers in your password ?");
 }
+//minimun count for numbers, lowercase, upercase and specailcharacters
+var minimumCOunt = 0;
+
+//Empty minimums for numbers, liwerCase, upperCases & special Characters
+
+var minimumNumbers = "";
+var minimumLoweCases = "";
+var minimumUpperCases = "";
+var minimumSpecialCharaters = "";
